b33760ddc871a349694fae112191070ae08612af
ajh123 <41990982+ajh123@users.noreply.github.com>
2020-10-14 16:08:43 +0100

    Update main.c

--- main.c
+++ main.c
@@ -24,80 +24,69 @@ void kprintf(char *str){
 
 void main()
 {
-    unsigned int cluster;
-    char c;
+	unsigned int cluster;
+	char c;
 
-    // set up serial console and linear frame buffer
-    uart_init();
-    lfb_init();
+	// set up serial console and linear frame buffer
+	uart_init();
+	lfb_init();
 
-    // display an ASCII string on screen with PSF
-	//UsbInitialise();
-    lfb_proprint(0, 0, "Loading GT-MOS");
-    wait_msec(1000000);
-    lfb_proprint(0, 0, "Loading GT-MOS [OK]");
-    lfb_proprint(0, 1*h, "Init EMMC");
+	lfb_proprint(0, 0, "Loading GT-MOS");
+	wait_msec(1000000);
+	lfb_proprint(0, 0, "Loading GT-MOS [OK]");
+	lfb_proprint(0, 1*h, "Init EMMC");
 
-    // initialize EMMC and detect SD card type
-    if(sd_init()==SD_OK) {
-        // read the master boot record and find our partition
-        if(fat_getpartition()) {
-            // find out file in root directory entries
-            cluster=fat_getcluster("LICENC~1BRO");
-            if(cluster==0)
-                cluster=fat_getcluster("KERNEL8 IMG");
-            if(cluster) {
-                // read into memory
-                uart_dump(fat_readfile(cluster));
-            }
-	    lfb_proprint(0, 1*h, "Init EMMC [OK]");
-        } else {
-            uart_puts("FAT partition not found???\n");
-	    	lfb_proprint(0, 1*h, "Init EMMC [FAT PARTITION: Not Found]");
-        }
-    } else {
+	// initialize EMMC and detect SD card type
+	if(sd_init()==SD_OK) {
+		// read the master boot record and find our partition
+		if(fat_getpartition()) {
+			// find out file in root directory entries
+			cluster=fat_getcluster("LICENC~1BRO");
+
+			if(cluster==0){cluster=fat_getcluster("KERNEL8 IMG");}
+
+			if(cluster) {
+				// read into memory
+				uart_dump(fat_readfile(cluster));
+			}
+			lfb_proprint(0, 1*h, "Init EMMC [OK]");
+		} else {
+			uart_puts("FAT partition not found???\n");
+			lfb_proprint(0, 1*h, "Init EMMC [FAT PARTITION: Not Found]");
+		}
+	} else {
 		lfb_proprint(0, 1*h, "Init EMMC [FAIL]");
-    }
+	}
 
 
-    // get the board's unique serial number with a mailbox call
-    mbox[0] = 8*4;                  // length of the message
-    mbox[1] = MBOX_REQUEST;         // this is a request message
-    
-    mbox[2] = MBOX_TAG_GETSERIAL;   // get serial number command
-    mbox[3] = 8;                    // buffer size
-    mbox[4] = 8;
-    mbox[5] = 0;                    // clear output buffer
-    mbox[6] = 0;
+	// get the board's unique serial number with a mailbox call
+	mbox[0] = 8*4;                  // length of the message
+	mbox[1] = MBOX_REQUEST;         // this is a request message
 
-    mbox[7] = MBOX_TAG_LAST;
+	mbox[2] = MBOX_TAG_GETSERIAL;   // get serial number command
+	mbox[3] = 8;                    // buffer size
+	mbox[4] = 8;
+	mbox[5] = 0;                    // clear output buffer
+	mbox[6] = 0;
 
-    // send the message to the GPU and receive answer
-    if (mbox_call(MBOX_CH_PROP)) {
+	mbox[7] = MBOX_TAG_LAST;
+
+	// send the message to the GPU and receive answer
+	if (mbox_call(MBOX_CH_PROP)) {
 		lfb_proprint(0, 2*h, "Serial Number:");
 
 		char str[80];
 		sprintf(str, "hex>: '%8x', '%8x'\n", mbox[6], mbox[5]);
 		lfb_proprint(0, 3*h, str);
 
-        uart_puts("My serial number is: ");
-        uart_hex(mbox[6]);
-        uart_hex(mbox[5]);
-        uart_puts("\n");
-    } else {
+		uart_puts("My serial number is: ");
+		uart_hex(mbox[6]);
+		uart_hex(mbox[5]);
+		uart_puts("\n");
+	} else {
 		uart_puts("Unable to query serial!\n");
 		lfb_proprint(0, 2*h, "Unable to find serial number");
-    }
-
-	/* Detect the first keyboard on USB bus */
-	//uint8_t firstKbd = 0;
-	//for (int i = 1; i <= MaximumDevices; i++) {
-	//	if (IsKeyboard(i)) {
-	//		firstKbd = i;
-	//		break;
-	//	}
-	//}
-	//if (firstKbd) lfb_proprint(0, 3*h,"Keyboard detected\r\n");
+	}
 
 	char *str = "                                ";
 	lfb_print(0, 0*h, str);
@@ -108,12 +97,11 @@ void main()
 
 	line = 0;
 	kprintf("Welcome to GT-MOS!");
-	kprintf("root@pi / $ ");
-    while(1) {
+	kprintf("root@pi / $ "); //// Read the keyboard here \\\\
+	while(1) {
 		// echo everything back
-        //uart_puts(" 1 - power off\n 2 - reset\nChoose one: ");
-        c=uart_getc();
-        uart_send(c);
+		c=uart_getc();
+		uart_send(c);
 
-    }
+	}
 }
