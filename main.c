da658c98db2832b6909665166f1f9220da2cdfd2
ajh123 <41990982+ajh123@users.noreply.github.com>
2020-10-15 16:23:26 +0100

    Update main.c

--- main.c
+++ main.c
@@ -108,7 +108,7 @@ void main()
 		kprintf("root@pi / $ "); //// Read the keyboard here \\\\
 		
 		if(line >= 50){ // Just to clear the screen for scrolling
-			line = 0
+			line = 0;
 			for (int i = 0; i < 50; i++) {
 				char *str = "                                                                                                ";
 				lfb_print(0, i*h, str);				
